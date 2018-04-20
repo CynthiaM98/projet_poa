bc8038161245b284d0cd00f72cfa5e1b82111c9a
Kiraya <c.maid9425@gmail.com>
2018-04-20 21:55:18 +0200

    "test push3"

--- src/Jeu.java
+++ src/Jeu.java
@@ -7,7 +7,6 @@ public class Jeu {
     private boolean isFound;
     private int xTresor;
     private int yTresor;
-    private int test;
     private PriorityQueue<Chasseur> listChasseur;
 
     public Jeu(int width, int height){
